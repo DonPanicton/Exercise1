242cf4029c9f2e92424fb4aae85d436a870f9157
DonPanicton <Franke_Florian@live.de>
2017-11-03 13:25:25 +0100

    Added HelloWorld Class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.print("Please enter your name: ");
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		HelloUser user = new HelloUser(s);
+		user.greetUser();
+		scan.close();
 	}
 
 }
